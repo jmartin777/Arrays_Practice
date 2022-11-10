d15b15c6d97ec66413daa762fc8559086a7fff48
Joshua Martin <ursa8minority@gmail.com>
2022-11-09 18:58:23 -0700

    Add methods to strings

--- arrays.js
+++ arrays.js
@@ -1,8 +1,25 @@
-// Declare a variable that stores an Array of at least 4 Strings.
+
+
+// 1. Declare a variable that stores an Array of at least 4 Strings.
+
 var birthMonths = ["July", "May", "October", "August"];
+// Method : sort() . This method sorts the elements in this array.(Sort order depends in implenation)
+birthMonths.sort()
 
-// Declare a variable that stores an Array of at least 4 Numbers.
+console.log(birthMonths)
+
+// 2. Declare a variable that stores an Array of at least 4 Numbers.
+
 var ticketPrices = [87, 88, 103, 68];
+// Method : pop() . This method removes the last element of an array
+ticketPrices.pop()
 
-// Declare a variable that stores an Array of at least 4 Booleans.
-var questions = [Truthful, Bad, Tall,Short];
+console.log(ticketPrices)
+
+// 3. Declare a variable that stores an Array of at least 4 Booleans.
+
+var questions = ["Truthful", "Bad", "Tall,Short"];
+// Method : reverse() . This method reverses the order of elements with last starting first etc.
+questions.reverse()
+
+console.log(questions);
