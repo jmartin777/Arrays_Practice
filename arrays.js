0150892fc9a5e62d9b85ff281ee1116c2b9bb0c3
Joshua Martin <ursa8minority@gmail.com>
2022-11-09 18:44:33 -0700

    Add 3 sets of arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+// Declare a variable that stores an Array of at least 4 Strings.
+var birthMonths = ["July", "May", "October", "August"];
+
+// Declare a variable that stores an Array of at least 4 Numbers.
+var ticketPrices = [87, 88, 103, 68];
+
+// Declare a variable that stores an Array of at least 4 Booleans.
+var questions = [Truthful, Bad, Tall,Short];
